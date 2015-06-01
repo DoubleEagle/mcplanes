a3cb2b3541573279f377d46d87108727bcdf744b
Joanne Spijker <eagle11.1.010@gmail.com>
2015-06-02 00:49:15 +0200

    Added clouds

--- js/environment.js
+++ js/environment.js
@@ -1,3 +1,33 @@
-function Environment() {
+function Environment(manager) {
     
+    manager.load('models/cloud.json', function(cloud) {
+        cloud.position.x = 5;
+        cloud.position.y = 10;
+        cloud.position.z = 10;
+        cloud.material = new THREE.MeshPhongMaterial(
+                {color: 0xFFFFFF, transparent: true, opacity: 0.5, specular: 0xFFFFFF, shininess: 8}
+        );
+        console.log('hoi')
+    });
+    
+    manager.load('models/cloud.json', function(cloud) {
+        cloud.position.x = 5;
+        cloud.position.y = -40;
+        cloud.position.z = 10;
+        cloud.material = new THREE.MeshPhongMaterial(
+                {color: 0xFFFFFF, transparent: true, opacity: 0.5, specular: 0xFFFFFF, shininess: 8}
+        );
+        console.log('hoi')
+    });
+    
+    manager.load('models/cloud.json', function(cloud) {
+        cloud.position.x = -20;
+        cloud.position.y = -30;
+        cloud.position.z = 10;
+        cloud.material = new THREE.MeshPhongMaterial(
+                {color: 0xFFFFFF, transparent: true, opacity: 0.5, specular: 0xFFFFFF, shininess: 8}
+        );
+        console.log('hoi')
+    });
+
 }
