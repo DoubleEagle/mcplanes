d3e89ec6f87c55df4b7f6eb284574148b42efdea
TempestasLudi <tempestasludi@gmail.com>
2015-06-04 19:14:17 +0200

    Increased                                                                           view distance (yap, that's a hyperbaton)

--- js/manager.js
+++ js/manager.js
@@ -32,7 +32,7 @@ function Manager(){
 	
 	function init3d(){
 		this.scene = new THREE.Scene();
-		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5000 );
 
 		this.renderer = new THREE.WebGLRenderer();
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
